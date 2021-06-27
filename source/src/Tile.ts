1378093f9e3ab3cca304291b1dce42bc69ac3e0f
Robby-the-paladin <71353456+Robby-the-paladin@users.noreply.github.com>
2021-06-27 23:44:32 +0300

    tile images works

--- source/src/Tile.ts
+++ source/src/Tile.ts
@@ -13,11 +13,9 @@ export enum ColisionType {
 export class Tile {
     public colision = ColisionType.Empty;
     public image : HTMLImageElement;
-
-
-    constructor(colision : ColisionType)/*, image : HTMLImageElement)*/ {
+    
+    constructor(colision : ColisionType) {
         this.colision = colision;
-        //this.image = image;
         if (colision == 0) {
             this.image = dr.Draw.loadImage("textures/Empty.png");
         }
@@ -40,24 +38,9 @@ export class Tile {
 
     public setColision(colision : ColisionType) {
         this.colision = colision;
-        //this.image = image;
-        if (colision == 0) {
-            this.image = dr.Draw.loadImage("textures/Empty.png");
-        }
-        if (colision == 1) {
-            this.image = dr.Draw.loadImage("textures/CornerUL.png");
-        }
-        if (colision == 2) {
-            this.image = dr.Draw.loadImage("textures/CornerUR.png");
-        }
-        if (colision == 3) {
-            this.image = dr.Draw.loadImage("textures/CornerDL.png");
-        }
-        if (colision == 4) {
-            this.image = dr.Draw.loadImage("textures/CornerDR.png");
-        }
-        if (colision == 5) {
-            this.image = dr.Draw.loadImage("textures/Full.png");
-        }
+    }
+
+    public setImage(image : HTMLImageElement) {
+        this.image = image;
     }
 }
