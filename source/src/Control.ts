ffa02526ddad5ae46061152bfc02313639459beb
StarikTenger <StarikTenger@gmail.com>
2021-08-15 19:47:15 +0300

    Control: wheel, mousePos

--- source/src/Control.ts
+++ source/src/Control.ts
@@ -12,6 +12,8 @@ export class Control {
     private static  keyMapping : Map<number, string[]>;
     private static _keys : boolean[] = [];
     private static clicked = false;
+    private static currentMousePos = new geom.Vector();
+    private static mouseWheelDelta = 0;
     private static commandsCounter : Map<string, number>;
     public static commands : Commands;
 
@@ -49,6 +51,8 @@ export class Control {
         window.addEventListener("keydown", Control.onKeyDown);
         window.addEventListener("keyup", Control.onKeyUp);
         window.addEventListener("click", Control.onClick);
+        window.addEventListener("wheel", Control.onWheel);
+        window.addEventListener("mousemove", Control.onMouseMove);
         
         console.log("lets do it!!");
         
@@ -74,7 +78,17 @@ S
 
     public static lastMouseCoordinates() : geom.Vector {
         Control.clicked = false;
-        return Control.commands.pointer;
+        return Control.commands.pointer.clone();
+    }
+
+    public static wheelDelta() : number {
+        let delta = this.mouseWheelDelta;
+        this.mouseWheelDelta = 0;
+        return delta;
+    }
+
+    public static mousePos() : geom.Vector {
+        return this.currentMousePos.clone();
     }
 
     private static onKeyDown(event : KeyboardEvent) : boolean {
@@ -120,6 +134,16 @@ S
         Control.commands.pointer = new geom.Vector(event.x, event.y);
         event.preventDefault();
         event.stopPropagation();
+        return false;
+    }
+
+    private static onWheel(event : WheelEvent) : boolean {
+        Control.mouseWheelDelta = event.deltaY;
+        return false;
+    }
+
+    private static onMouseMove(event : MouseEvent) : boolean {
+        Control.currentMousePos = new geom.Vector(event.x, event.y);
         return false;
     }
 }
