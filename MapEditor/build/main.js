aba0c5495172aa74a3edf2fe2397bfd480dc0db0
StarikTenger <StarikTenger@gmail.com>
2021-08-19 18:28:35 +0300

    удалена папка mapEditor

--- MapEditor/build/main.js
+++
@@ -1,7 +0,0 @@
-// Set up any config you need (you might not need this)
-requirejs.config({
-    basePath: "/scripts"
-  });
-  
-  // Tell RequireJS to load your main module (and its dependencies)
-  require("mainmodule");
