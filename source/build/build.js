e5e4883bf215bdfff28fecb9fa286dad1869b12d
ter <misha37a999@yandex.ru>
2021-08-20 23:11:39 +0300

    асинхронная загрузка анимаций

--- source/build/build.js
+++ source/build/build.js
@@ -1,18 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -49,6 +34,21 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 define("Geom", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -139,6 +139,7 @@ define("Draw", ["require", "exports"], function (require, exports) {
             if (this.images[src]) {
                 return this.images[src];
             }
+            console.log("getNewImage");
             var image = new Image();
             image.src = src;
             this.images[src] = image;
@@ -262,7 +263,29 @@ define("Entities/EntityAttributes/Animation", ["require", "exports", "Draw"], fu
             this.current_state = Draw_1.Draw.loadImage("textures/" + this.name + "/right_fine_" + this.counter % this.states + ".png");
             this.mode = "fine";
             this.direction = "right";
+            this.Imageloader();
         }
+        Animation.prototype.Imageloader = function () {
+            return __awaiter(this, void 0, void 0, function () {
+                var direction, mods, mod, direct, _i;
+                return __generator(this, function (_a) {
+                    direction = ["top", "down", "left", "right", "stand"];
+                    mods = ["corrupted", "dying", "fine"];
+                    for (mod in mods) {
+                        for (direct in direction) {
+                            for (_i = 0; _i < this.states; _i++) {
+                                this.getImage("textures/" +
+                                    this.name + "/" +
+                                    direct + "_" +
+                                    mod + "_" +
+                                    _i + ".png");
+                            }
+                        }
+                    }
+                    return [2];
+                });
+            });
+        };
         Animation.prototype.getImage = function (current) {
             return Draw_1.Draw.loadImage(current);
         };
